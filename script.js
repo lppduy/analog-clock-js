6efdfca1ca4fde610693d085ef39221baf006dbf
user <user@Eno-ITs-MacBook-Pro.local>
2023-11-16 17:14:31 +0700

    done function, start to implement the UI

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const secondHand = document.getElementById('second-hand');
 const minuteHand = document.getElementById('minute-hand');
 const hourHand = document.getElementById('hour-hand');
 const digitalElement = document.getElementById('digital');
+const dateElement = document.getElementById('date');
 
 function clockTick() {
   const date = new Date();
@@ -16,19 +17,58 @@ function clockTick() {
   rotateClockHand(minuteHand, minutes);
   rotateClockHand(hourHand, hours);
 
-  const digitalSec = formatDigital(date.getSeconds());
   const digitalMin = formatDigital(date.getMinutes());
-  const digitalHour = formatDigital(date.getHours());
+  const digitalHour = date.getHours() === 12 ? 12 : date.getHours() % 12;
 
-  digitalElement.innerHTML = `${digitalHour}:${digitalMin}:${digitalSec}`;
+  digitalElement.innerHTML = `${digitalHour}:${digitalMin}`;
+
+  const formattedDate = getFormattedDate();
+
+  dateElement.innerHTML = formattedDate;
 }
+
+// helper functions:
 
 function rotateClockHand(el, rotation) {
   el.style.setProperty('--rotate', rotation * 360);
 }
+
+function getFormattedDate() {
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const date = new Date();
+
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  const dayOfMonth = date.getDate();
+  const month = months[date.getMonth()];
+
+  return `${dayOfWeek}, ${month} ${dayOfMonth}`.toUpperCase();
+}
+
 function formatDigital(time) {
   return time < 10 ? `0${time}` : time;
 }
 
-// Gọi hàm cập nhật mỗi giây
 setInterval(clockTick, 1000);
